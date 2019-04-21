1aba2da60c4b2d1e500173d755a816cda1df0c41
admin <admin@example.com>
2019-04-21 17:13:14 +0800

    User authentication with Flask-Login

--- models.py
+++ models.py
@@ -2,12 +2,31 @@
 # Time    : 2019/4/21 14:44
 # Author  : LiaoKong
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from utils import db
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def is_active(self):
+        return True
+
+    def get_id(self):
+        return self.id
+
+    def is_authenticated(self):
+        return True
 
 
 class Movie(db.Model):
@@ -15,9 +34,9 @@ class Movie(db.Model):
     title = db.Column(db.String(60))
     year = db.Column(db.String(4))
 
-
+# db.drop_all()
 # db.create_all()
-# name = 'Grey Li'
+# name = 'LiaoKong'
 # movies = [
 #     {'title': 'My Neighbor Totoro', 'year': '1988'},
 #     {'title': 'Dead Poets Society', 'year': '1989'},
@@ -30,13 +49,13 @@ class Movie(db.Model):
 #     {'title': 'WALL-E', 'year': '2008'},
 #     {'title': 'The Pork of Music', 'year': '2012'},
 # ]
-
-
-# user = User(name=name)
+#
+# user = User(name=name, username=name)
+# user.set_password("123456")
 # db.session.add(user)
 #
 # for m in movies:
-#     movie = Movie(title=m["title"],year=m["year"])
+#     movie = Movie(title=m["title"], year=m["year"])
 #     db.session.add(movie)
 #
 # db.session.commit()
